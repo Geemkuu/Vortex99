58fd98d2d0555d188638e45ff309cf665d9ec8d5
Geemkuu <144843941+Geemkuu@users.noreply.github.com>
2024-03-17 14:59:52 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,31 +16,31 @@ global.socialm = "GitHub: Wota777FF" //ur github or insta name
 global.location = "Mena, Morocco, Casablanca" //ur location
 
 //new
-global.botname = 'Vortex-99' //ur bot name
-global.ownernumber = '918360234087' //ur owner number
-global.ownername = 'ＷㅤＨㅤＩㅤＴㅤＥㅤ４ㅤ４ㅤ４    ?    ?' //ur owner name
+global.botname = '亗『ＧＥＥ』亗' //ur bot name
+global.ownernumber = '254757422843' //ur owner number
+global.ownername ='𝕲⃟𝕰⃟𝕰⃟' //ur owner name
 global.websitex = "https://youtu.com/@WHITE444_YT"
-global.wagc = "https://whatsapp.com/channel/0029VaSFe9J7oQhb9v6nqC2G"
-global.themeemoji = '🐰'
-global.wm = "ＷㅤＨㅤＩㅤＴㅤＥㅤ４ㅤ４ㅤ４  ?  ?"
-global.botscript = 'https://github.com/Wota777FF/Vortex99' //script link
+global.wagc = "https://chat.whatsapp.com/DSQwxQR6Nww8cM5SDZbLNl"
+global.themeemoji = '😂'
+global.wm = "G͜͡E͜͡E͜͡"
+global.botscript = 'https://github.com/Geemkuu/Vortex99' //script link
 global.packname = "Powered by"
-global.author = "Vortex 99"
-global.creator = "918360234087@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["918360234087"] // Premium User
+global.author = "𝑔𝑒𝑒"
+global.creator = "254794286079@s.whatsapp.net"
+global.xprefix = '¶'
+global.premium = ["254757422843"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v4' // menu type 'v1' => 'v8'
-global.typereply = 'v2' // reply type 'v1' => 'v3'
+global.typemenu = 'v7' // menu type 'v1' => 'v8'
+global.typereply = 'v3' // reply type 'v1' => 'v3'
 global.autoblocknumber = '234' //set autoblock country code
 global.antiforeignnumber = '234' //set anti foreign number country code
-global.welcome = true //welcome/left in groups
+global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = true //auto status/story view
-global.adminevent = true //show promote/demote message
-global.groupevent = true //show update messages in group chat
+global.adminevent = false //show promote/demote message
+global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
 	limit: '*Limit Exceeded*',
